58ee07e42db941f6fcaa517f9e11fb48a36af908
nageswari15 <sivanageswariputta@gmail.com>
2025-07-04 00:22:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import React from "react";
+import React from "./style.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>    <h1>To-Do app</h1>
+    <div className="demo">
+      <Home />
+    </div>
+    </>
+  );
+}
+
+export default App;
